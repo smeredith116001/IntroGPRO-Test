e02c3d78cf0ede97e86d0171e24b3904c15939c2
Sebastian Meredith <sebastian.meredith@mymail.champlain.edu>
2020-04-10 16:30:35 -0400

    Started Framework Libary

--- GPRO Framework/source/GPRO-FW-Launcher/GPRO-FW-Launcher_main.c
+++ GPRO Framework/source/GPRO-FW-Launcher/GPRO-FW-Launcher_main.c
@@ -1,7 +1,17 @@
 // GPRO-FW-Launcher.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+// declaration/prototype/interface
+int foo(int bar);
+// defintion implentation
+int foo(int bar)
+{
+	return (bar + 1);
+}
+#include <stdio.h>
+#include <stdlib.h>
 int main()
 {
-
+	int test = foo(9000);
+	printf("\n %d \n", test);
+	system("pause");
 }
